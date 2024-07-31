2c75696d4eab38b10a397c2aecf7b8bc1e5319fa
Daniel Kalivoda <danek.kalivoda@hey.com>
2024-07-31 16:52:50 +0200

    resurrection

---
+++ config/plugins.js
@@ -0,0 +1,38 @@
+// ./config/plugins.js`
+'use strict';
+
+module.exports = {
+  menus: {
+    config: {
+      maxDepth: 2,
+      layouts: {
+        menuItem: {
+          link: [
+            {
+              input: {
+                label: 'Hidden',
+                name: 'link_hidden',
+                type: 'bool',
+              },
+              grid: {
+                col: 6,
+              },
+            },
+            {
+              input: {
+                label: 'Connected page',
+                name: 'page_relation',
+                type: 'relation',
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 10,
+    }
+  },
+};
